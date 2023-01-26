946d5e808edcd0caf1a4b232f2a4a351ff0bb275
tristan <thuckabee.work@gmail.com>
2023-01-25 22:28:43 -0600

    set up server

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,13 @@ const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const routes = require('./routes');
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(routes);
 
 //! Security Middleware
 if( !isProduction ) {
@@ -32,3 +34,8 @@ app.use(
     }
   })
 );
+
+
+
+// ---------------------------------------------------------
+module.exports = app;
