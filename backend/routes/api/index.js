f03fdbe5c24f1db725dc9eb049076446840894b2
tristan <thuckabee.work@gmail.com>
2023-01-27 23:14:27 -0600

    Add User Auth Express Middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,20 +1,13 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
 const sessionRouter = require('./session');
 const userRouter = require('./users');
 
-router.get('/csrf/restore', (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie('XSRF-TOKEN', csrfToken);
-  res.status(200).json({'XSRF-Token': csrfToken});
-});
+router.use(restoreUser);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-router.get('/getImageData', (req, res) => {
-  res.setHeader('Content-type', 'text/html');
-  res.send(`<input type="file" /><script>let input = document.querySelectorAll('input')[0];input.addEventListener(onchange, () =>console.log(input.value))</script>`)
-})
 
 // --------------------------------------------------------
 module.exports = router;
