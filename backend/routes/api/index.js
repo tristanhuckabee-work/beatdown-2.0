a47e82415afa167a7e7358a46406a7eee30b0959
tristan <thuckabee.work@gmail.com>
2023-01-29 08:51:08 -0600

    Added API Router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+// ----------------------------------------------------------------------------
+module.exports = router;
